68c4ec6f6dd4d29a1d83b0d31c93cbfb57e3d06f
Steven Leder <steven.leder@bauzentrum-loeffler.de>
2023-05-11 10:33:37 +0200

    AzureRescourceserver Project  added

---
+++ src/main/java/com/leit/bzl/intranet/rescources/azurerescourceserver/azurerescourceserver/AzureRescourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.leit.bzl.intranet.rescources.azurerescourceserver.azurerescourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureRescourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureRescourceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leit/bzl/intranet/rescources/azurerescourceserver/azurerescourceserver/AzureRescourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leit.bzl.intranet.rescources.azurerescourceserver.azurerescourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureRescourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
